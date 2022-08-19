4ece6712cdbc85c313b54e277b49762dc981e59b
Aria Seyedahmadi <ariaseyedahmadi.2015@gmail.com>
2022-08-20 03:33:20 +0430

    updated commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using PeriodicTableSearch.Data;
+
+using var db = new PeriodicElementsDbContext();
+
+static void FindElement(PeriodicElementsDbContext? db)
+{
+    Console.WriteLine("List of all elements:");
+    var list = db.PeriodicElements.ToList();
+    
+    foreach(var element in list)
+    {
+        Console.WriteLine($"{element.AtomicNumber} | {element.Element} | {element.Symbol}");
+    }
+}
+
+FindElement(db);
