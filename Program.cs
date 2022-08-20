076466907c5d59e05843c1083def905a81d63a2c
Aria Seyedahmadi <ariaseyedahmadi.2015@gmail.com>
2022-08-20 18:20:29 +0430

    printing the output in a table PrintOutput method

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ConsoleTables;
 using PeriodicTableSearch.Data;
 
 class Program
@@ -11,18 +11,42 @@ class Program
 
         var element = _context.PeriodicElements
             .Where(x => x.Symbol == symbol)
-            .FirstOrDefault();
+            .FirstOrDefault()!;
 
-        Console.WriteLine(element);
+        PrintOutput(element);
     }
 
     static void FindElementByAtomicNumber(String atomicNumber)
     {
         var element = _context.PeriodicElements
             .Where(x => x.AtomicNumber == atomicNumber)
-            .FirstOrDefault();
+            .FirstOrDefault()!;
 
-        Console.WriteLine(element);
+        PrintOutput(element);
+    }
+
+    static void PrintOutput(PeriodicElement element)
+    {
+        var table = new ConsoleTable(
+            "AtomicNumber",
+            "Element",
+            "Symbol",
+            "AtomicMass",
+            "Phase",
+            "Type"
+        );
+        table.AddRow(
+            element.AtomicNumber,
+            element.Element,
+            element.Symbol,
+            element.AtomicMass,
+            element.Phase,
+            element.Type
+        );
+
+        Console.Clear();
+        table.Write();
+        Console.WriteLine();
     }
 
     static void Main(string[] args)
